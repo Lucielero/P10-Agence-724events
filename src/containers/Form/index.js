f374508ce80b29ba3dbdd28eba2d90a53a1131dc
Lucie Leroux <lucieleroux93@gmail.com>
2024-10-25 13:24:05 +0200

    Correction formulaire de contact

--- src/containers/Form/index.js
+++ src/containers/Form/index.js
@@ -16,6 +16,7 @@ const Form = ({ onSuccess, onError }) => {
       try {
         await mockContactApi();
         setSending(false);
+        onSuccess(); // appel de la fonction onSuccess après envoi réussi
       } catch (err) {
         setSending(false);
         onError(err);
